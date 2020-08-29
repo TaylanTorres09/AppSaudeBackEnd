f052f7a49e09076da6ef835b334f9bb04c6c9a06
taylantorres09 <taylantorres@gmail.com>
2020-08-29 18:20:01 -0300

    Até ProfessionalData pronto

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,10 +9,16 @@ const PatientController = require('../controllers/patientController');
 const PatientAuth = require('../auth/patientAuth')
 const PatientRegister = require('../auth/patientRegister')
 
+//PatientData
+const PatientDataController = require('../controllers/patientDataController')
+
 //Professional
 const ProfessionalController = require('../controllers/professionalController')
 const ProfessionalAuth = require('../auth/professionalAuth')
 const ProfessionalRegister = require('../auth/professionalRegister')
+
+//ProfessionalData
+const ProfessionalDataController = require('../controllers/professionalDataController')
 
 
 routes
@@ -30,13 +36,22 @@ routes
     .get('/api/patient/:id', PatientController.getUserByID)
     .get('/api/patient', PatientController.getUserByEmail) // http://localhost:3000/api/patient?email=maods@maods.com
 
+    // PatientData
+    .get('/api/patient/data/:id', PatientDataController.getDataByUserId)
+    .post('/api/patient/data', PatientDataController.createData) 
+    .put('/api/patient/data/:id', PatientDataController.updateData)
+    
     // Professional
     .post('/api/professional/authentication', ProfessionalAuth.loginProfessional)
     .post('/api/professional/register', ProfessionalRegister.registerProfessional)
     .get('/api/professional/:id', PatientController.getUserByID)
-    .get('/api/patient', PatientController.getUserByEmail) // http://localhost:3000/api/professional?email=
+    .get('/api/professional', PatientController.getUserByEmail) // http://localhost:3000/api/professional?email=
 
-    
+    // ProfessionalData
+    .get('/api/professional/data/:id', ProfessionalDataController.getDataByUserId)
+    .post('/api/professional/data', ProfessionalDataController.createData) 
+    .put('/api/professional/data/:id', ProfessionalDataController.updateData)
+
 
 
 module.exports = routes
