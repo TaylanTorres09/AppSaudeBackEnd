cf2c39cff751afc9995368104ae62c7e687034ae
taylantorres09 <taylantorres@gmail.com>
2020-08-29 16:51:44 -0300

    rotas professional e patient completas

--- src/controllers/patientController.js
+++ src/controllers/patientController.js
@@ -9,11 +9,28 @@ module.exports = {
             const results = await Patient.find()
             // const results = await Product.find({}, { name: 1, price: 1, _id: 0 });
             // const results = await Product.find({ price: 699 }, {});
-            res.send(results);
+            res.send( results);
         } catch (error) {
             console.log(error.message);
         }
     },
+    
+    getUserByID: async (req, res) => {
+        try {
+            const results = await Patient.findById(req.params.id);
 
+            return res.send( results);
+        } catch (error) {
+            console.log(error.message);
+        }
+    },
+    getUserByEmail: async (req, res) => {
+        try {
+            const results = await Patient.findOne({email: req.query.email});
+            return res.json(results);
 
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,30 +1,43 @@
 const express = require('express')
 const routes = express.Router()
 
+//Middleware - Proteção
 const authJwt = require('../middleware/authJwt');
 
-const PatientController = require('../controllers/patientController');
-
 //Patient
+const PatientController = require('../controllers/patientController');
 const PatientAuth = require('../auth/patientAuth')
 const PatientRegister = require('../auth/patientRegister')
 
 //Professional
+const ProfessionalController = require('../controllers/professionalController')
 const ProfessionalAuth = require('../auth/professionalAuth')
 const ProfessionalRegister = require('../auth/professionalRegister')
 
 
 routes
-    // Users
-    .get('/', PatientController.getAll)
-    .get('/test', [authJwt.verifyToken], PatientController.getAll)
+    // Users - Patient
+    .get('/all/patient', PatientController.getAll)
+    .get('/all/patient/test', [authJwt.verifyToken], PatientController.getAll) // Rota protegida
+
+    // Users - Professional
+    .get('/all/professional', ProfessionalController.getAll)
+    .get('/all/patient/test', [authJwt.verifyToken], ProfessionalController.getAll) // Rota protegida
 
     // Patient
     .post('/api/patient/authentication', PatientAuth.loginPatient)
     .post('/api/patient/register', PatientRegister.registerPatient)
+    .get('/api/patient/:id', PatientController.getUserByID)
+    .get('/api/patient', PatientController.getUserByEmail) // http://localhost:3000/api/patient?email=maods@maods.com
+
     // Professional
     .post('/api/professional/authentication', ProfessionalAuth.loginProfessional)
     .post('/api/professional/register', ProfessionalRegister.registerProfessional)
+    .get('/api/professional/:id', PatientController.getUserByID)
+    .get('/api/patient', PatientController.getUserByEmail) // http://localhost:3000/api/professional?email=
+
+    
+
 
 module.exports = routes
 
